10e9bb477daca01e8b28e81eabd6fb7348d80908
Matheus Santos <matheus.r.d.s10@hotmail.com>
2026-05-15 17:51:10 -0300

    improve github release

--- scripts/gh-release.sh
+++ scripts/gh-release.sh
@@ -1,14 +1,36 @@
 #!/bin/bash -e
 
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+# VERSION=$(node -p "require('$ROOT_DIR/package.json').version")
+VERSION=0.0.2
+# ANDROID_TAG="v${VERSION}-android"
+# IOS_TAG="v${VERSION}-ios"
+TAG="v${VERSION}"
+
 # android release
-cd ./android
+echo "Building Android AAR..."
+cd "$ROOT_DIR/android"
 ./gradlew assembleRelease
-# TODO: dynamically get version
-gh release create v0.0.1-android ./build/outputs/aar/android-release.aar -t "CapLlama Android Archive v0.0.1"
+# gh release create "$ANDROID_TAG" ./build/outputs/aar/android-release.aar -t "CapLlama Android Archive ${VERSION}"
 
 # ios release
-cd ../ios
+echo "Building iOS XCFramework..."
+cd "$ROOT_DIR/ios"
 zip -r capllama.xcframework.zip ./Sources/CapacitorLlamaPlugin/capllama.xcframework
-gh release create v0.0.1-ios capllama.xcframework.zip -t "CapLlama iOS Framework v0.0.1"
-rm -f capllama.xcframework.zip
+# gh release create "$IOS_TAG" capllama.xcframework.zip -t "CapLlama iOS Framework ${VERSION}"
 
+cd "$ROOT_DIR"
+
+mv android/build/outputs/aar/android-release.aar ./
+mv ios/capllama.xcframework.zip ./
+
+echo "Creating GitHub release $TAG..."
+gh release create "$TAG" \
+  android-release.aar \
+  capllama.xcframework.zip \
+  -t "CapLlama ${TAG}"
+
+rm -f capllama.xcframework.zip
+rm -f android-release.aar
+
+echo "Release $TAG created successfully!"
