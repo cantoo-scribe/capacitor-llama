890b804de2770843354879de99021b9687f771aa
Matheus Santos <matheus.r.d.s10@hotmail.com>
2026-05-22 08:29:05 -0300

    feat: use postinstall to delivery native libs

--- scripts/gh-release.sh
+++ scripts/gh-release.sh
@@ -3,34 +3,29 @@
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 # VERSION=$(node -p "require('$ROOT_DIR/package.json').version")
 VERSION=0.0.2
-# ANDROID_TAG="v${VERSION}-android"
-# IOS_TAG="v${VERSION}-ios"
 TAG="v${VERSION}"
 
 # android release
-echo "Building Android AAR..."
-cd "$ROOT_DIR/android"
-./gradlew assembleRelease
-# gh release create "$ANDROID_TAG" ./build/outputs/aar/android-release.aar -t "CapLlama Android Archive ${VERSION}"
+echo "Compressing Android JNI libs..."
+zip -r android/src/main/capllama-android-jni-libs.zip ./android/src/main/jniLibs
 
 # ios release
-echo "Building iOS XCFramework..."
+echo "Compressing iOS XCFramework..."
 cd "$ROOT_DIR/ios"
 zip -r capllama.xcframework.zip ./Sources/CapacitorLlamaPlugin/capllama.xcframework
-# gh release create "$IOS_TAG" capllama.xcframework.zip -t "CapLlama iOS Framework ${VERSION}"
 
 cd "$ROOT_DIR"
 
-mv android/build/outputs/aar/android-release.aar ./
+mv android/src/main/capllama-android-jni-libs.zip ./
 mv ios/capllama.xcframework.zip ./
 
 echo "Creating GitHub release $TAG..."
 gh release create "$TAG" \
-  android-release.aar \
+  capllama-android-jni-libs.zip \
   capllama.xcframework.zip \
   -t "CapLlama ${TAG}"
 
-rm -f capllama.xcframework.zip
-rm -f android-release.aar
+# rm -f capllama.xcframework.zip
+# rm -f capllama-android-jni-libs.zip
 
 echo "Release $TAG created successfully!"
